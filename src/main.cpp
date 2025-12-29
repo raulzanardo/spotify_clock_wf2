80901c98749739022c4f11e48d89097aee1586e2
raul.zanardo <raul.zanardo@petpoint.com>
2025-12-29 11:54:43 -0300

    added boot log and fail safe for internet been down at boot

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,15 @@
 #include <Fonts/FreeSans12pt7b.h>
 
 #define countof(x) (sizeof(x) / sizeof(x[0]))
+#define SETUP_LOG_LINES 8
 
 // Function prototypes
 uint16_t getClockDigitColor(int hour, int minute);
 void drawClock(const String &clockText, uint16_t bodyColor, int yOffset = 39);
+bool hasInternetConnectivity();
+void ensureSpotifyReady();
+void addSetupLog(const String &msg);
+void drawSetupLogs();
 
 #ifdef ENABLE_CALENDAR
 String fetchCalendar();
@@ -29,8 +34,6 @@ void drawCalendarLines(const String &calendarText, uint16_t bodyColor, int start
 int downloadImage(const String &imageUrl);
 int drawMCU(JPEGDRAW *pDraw);
 void drawJPEG(const char *filename, int xpos, int ypos);
-void setup();
-void loop();
 
 ///////////////////////////////////////////////////////////////////////////////////
 // MatrixPanel_I2S_DMA dma_display;
@@ -39,6 +42,10 @@ MatrixPanel_I2S_DMA *dma_display = nullptr;
 String currentAlbumArtUrl = "";
 String previousAlbumArtUrl = " ";
 bool isSpotifyPlaying = false;
+bool spotifyInitialized = false;
+bool spotifyAuthenticated = false;
+String setupLogs[SETUP_LOG_LINES];
+int setupLogCount = 0;
 #ifdef ENABLE_CALENDAR
 unsigned long lastCalendarFetch = 0;
 String lastCalendarResponse = "";
@@ -224,7 +231,125 @@ void drawClock(const String &clockText, uint16_t bodyColor, int yOffset)
 
     dma_display->setCursor(xOffset, yOffset);
     dma_display->setTextColor(bodyColor);
-    dma_display->printf(clockText.c_str());
+    dma_display->printf("%s", clockText.c_str());
+}
+
+bool hasInternetConnectivity()
+{
+    HTTPClient http;
+    http.setConnectTimeout(3000);
+    http.setTimeout(3000);
+
+    // Lightweight connectivity check endpoint
+    if (!http.begin("http://clients3.google.com/generate_204"))
+    {
+        USBSerial.println(F("Connectivity check begin failed"));
+        return false;
+    }
+
+    int code = http.GET();
+    http.end();
+    bool ok = code == 204;
+    if (ok)
+    {
+        USBSerial.println(F("Internet reachable"));
+    }
+    else
+    {
+        USBSerial.println("No internet, code: " + String(code));
+    }
+    return ok;
+}
+
+void ensureSpotifyReady()
+{
+    if (spotifyAuthenticated)
+        return;
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        USBSerial.println(F("WiFi lost, cannot init Spotify"));
+        spotifyInitialized = false;
+        return;
+    }
+
+    if (!hasInternetConnectivity())
+    {
+        USBSerial.println(F("No internet, deferring Spotify auth"));
+        return;
+    }
+
+    if (!spotifyInitialized)
+    {
+        USBSerial.print(F("Spotify begin: "));
+        sp.begin();
+        spotifyInitialized = true;
+        USBSerial.println(F("started"));
+    }
+
+    if (!sp.is_auth())
+    {
+        USBSerial.print(F("Authenticating Spotify (timeout 10s): "));
+        unsigned long start = millis();
+        while (!sp.is_auth() && millis() - start < 10000)
+        {
+            sp.handle_client();
+            delay(10);
+        }
+        if (sp.is_auth())
+        {
+            spotifyAuthenticated = true;
+            USBSerial.printf("Authenticated! Refresh token: %s\n", sp.get_user_tokens().refresh_token);
+        }
+        else
+        {
+            USBSerial.println(F("Auth not completed, will retry later"));
+        }
+    }
+    else
+    {
+        spotifyAuthenticated = true;
+        USBSerial.println(F("Spotify already authenticated"));
+    }
+}
+
+void addSetupLog(const String &msg)
+{
+    if (setupLogCount < SETUP_LOG_LINES)
+    {
+        setupLogs[setupLogCount++] = msg;
+    }
+    else
+    {
+        // shift up to keep the latest logs
+        for (int i = 1; i < SETUP_LOG_LINES; ++i)
+        {
+            setupLogs[i - 1] = setupLogs[i];
+        }
+        setupLogs[SETUP_LOG_LINES - 1] = msg;
+    }
+    drawSetupLogs();
+}
+
+void drawSetupLogs()
+{
+    if (dma_display == nullptr)
+        return;
+
+    dma_display->clearScreen();
+    dma_display->setFont(&Picopixel);
+    dma_display->setTextSize(1);
+    dma_display->setTextWrap(false);
+    dma_display->setTextColor(0xFFFF); // white
+
+    const int lineHeight = 8;
+    for (int i = 0; i < setupLogCount; ++i)
+    {
+        int y = (i + 1) * lineHeight;
+        dma_display->setCursor(0, y);
+        dma_display->printf("%s", setupLogs[i].c_str());
+    }
+    dma_display->flipDMABuffer();
 }
 
 int downloadImage(const String &imageUrl)
@@ -318,6 +443,7 @@ void setup()
     // Initialize USBSerial port for debugin
     USBSerial.begin(115200);
     USBSerial.println("\nSTART!");
+    addSetupLog("Booting...");
 
     // Initialize led matrix
     USBSerial.println(F("Led Matrix begin"));
@@ -338,20 +464,24 @@ void setup()
     dma_display->setBrightness8(30);
     dma_display->clearScreen();
     dma_display->flipDMABuffer();
+    addSetupLog("Display ready");
 
     // Initialize LittleFS
     USBSerial.print(F("LittleFS begin: "));
     if (!LittleFS.begin(true))
     {
         USBSerial.println(F("failed"));
+        addSetupLog("FS: failed");
     }
     else
     {
         USBSerial.println(F("ok"));
+        addSetupLog("FS: ok");
     }
 
     // Initialize Wifi
     USBSerial.print(F("WiFi begin: "));
+    addSetupLog("WiFi: connecting...");
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -364,12 +494,15 @@ void setup()
     if (WiFi.status() != WL_CONNECTED)
     {
         USBSerial.println(F("\nWiFi failed! Restarting..."));
+        addSetupLog("WiFi: failed");
         // ESP.restart();
     }
     else
     {
         USBSerial.println((String) "RSSI : " + WiFi.RSSI() + " dB");
         USBSerial.println("\nIP:" + WiFi.localIP().toString());
+        addSetupLog("WiFi: connected");
+        addSetupLog("IP: " + WiFi.localIP().toString());
     }
 
     WiFi.setAutoReconnect(true);
@@ -380,12 +513,14 @@ void setup()
     if (!MDNS.begin(PROJECTNAME))
     {
         USBSerial.println(F("failed"));
+        addSetupLog("mDNS: failed");
         // ESP.restart();
     }
     else
     {
         // Set the hostname to "$PROJECTNAME.local"
         USBSerial.println(F("ok"));
+        addSetupLog("mDNS: ok");
     }
 
     // Initialize NTPC
@@ -395,44 +530,45 @@ void setup()
     if (!getLocalTime(&timeinfo))
     {
         USBSerial.println(F("Failed to obtain time"));
+        addSetupLog("Time: failed");
         return;
     }
     USBSerial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    addSetupLog("Time: synced");
 
-    // Initialize Spotify
-    USBSerial.print(F("Spotify begin: "));
-    sp.begin(); // Start spotify service
-
-    if (!sp.is_auth())
-    {
-        USBSerial.print(F("Wait for the user to authenticate: "));
-        while (!sp.is_auth())
-        {
-            // Wait for the user to authenticate
-            sp.handle_client(); // Handle the client, this is necessary otherwise the webserver won't work
-        }
-        USBSerial.printf("Authenticated! Refresh token: %s\n", sp.get_user_tokens().refresh_token);
-    }
-    else
-    {
-        USBSerial.println(F("OK"));
-    }
-
-    // Draw clock
-    char datestring[6];
-    snprintf_P(datestring,
-               countof(datestring),
-               PSTR("%02u:%02u"),
-               timeinfo.tm_hour,
-               timeinfo.tm_min);
-    drawClock(datestring, getClockDigitColor(timeinfo.tm_hour, timeinfo.tm_min));
-    dma_display->flipDMABuffer();
+    ensureSpotifyReady(); // Will defer if no internet
+    addSetupLog(spotifyAuthenticated ? "Spotify: ok" : "Spotify: pending");
 }
 
 void loop()
 {
     dma_display->clearScreen();
     //-------------------------------------------------------------------------------
+    ensureSpotifyReady();
+
+    if (!spotifyAuthenticated)
+    {
+        USBSerial.println(F("Spotify not ready, showing clock only"));
+
+        if (!getLocalTime(&timeinfo))
+        {
+            USBSerial.println(F("Failed to obtain time"));
+        }
+
+        char datestring[6];
+        snprintf_P(datestring,
+                   countof(datestring),
+                   PSTR("%02u:%02u"),
+                   timeinfo.tm_hour,
+                   timeinfo.tm_min);
+
+        uint16_t bodyColor = getClockDigitColor(timeinfo.tm_hour, timeinfo.tm_min);
+        drawClock(datestring, bodyColor, 39);
+        dma_display->flipDMABuffer();
+        delay(4000);
+        return;
+    }
+
     USBSerial.println(F("Checking Spotify state"));
 
     response currentState = sp.currently_playing();
